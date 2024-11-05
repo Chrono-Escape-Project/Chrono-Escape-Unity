9d225886cf276013e5ed46b99a0a32b043b9c844
SparkyPX <pierrelucproulx@gmail.com>
2024-11-05 09:21:06 -0500

    ajout xr controller to playtest

--- Chrono Escape/Assets/Script/vrdetection.cs
+++ Chrono Escape/Assets/Script/vrdetection.cs
@@ -5,29 +5,36 @@ using UnityEngine.XR.Management;
 
 public class vrdetection : MonoBehaviour
 {
-
+    public bool activVR = true;
     public GameObject sourisClavier;
 
     // Start is called before the first frame update
     void Start()
     {
-        var xrParametres = XRGeneralSettings.Instance;
-        if (xrParametres == null)
+        if(activVR == true)
         {
-            return;
+            var xrParametres = XRGeneralSettings.Instance;
+            if (xrParametres == null)
+            {
+                return;
+            }
+            var xrManager = xrParametres.Manager;
+            if (xrManager == null)
+            {
+                return;
+            }
+            var xrLoader = xrManager.activeLoader;
+            if (xrLoader == null)
+            {
+                sourisClavier.SetActive(true);
+                return;
+            }
+            sourisClavier.SetActive(false);
         }
-        var xrManager = xrParametres.Manager;
-        if (xrManager == null)
-        {
-            return;
-        }
-        var xrLoader = xrManager.activeLoader;
-        if (xrLoader == null)
+        else
         {
             sourisClavier.SetActive(true);
-            return;
         }
-        sourisClavier.SetActive(false);
 
 
     }
