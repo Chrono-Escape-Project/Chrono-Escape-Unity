f25c8b94767158f6de638d9b10adbbf66b5f1730
SparkyPX <pierrelucproulx@gmail.com>
2024-11-26 09:07:34 -0500

    yes

--- Chrono Escape/Assets/Script/FlickerControls.cs
+++ Chrono Escape/Assets/Script/FlickerControls.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class FlickerControls : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public bool isFlickering = false;
+    public float timeDelay;
 
-    // Update is called once per frame
-    void Update()
+     void Update()
     {
-        
+        if ()
     }
 }
