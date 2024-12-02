bc9075bd362c9662a8cb62a3977abe818aca185c
SparkyPX <pierrelucproulx@gmail.com>
2024-12-02 10:57:18 -0500

    Added GameManager Script

---
+++ Chrono Escape/Assets/Script/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    private List<string> collectedCodes = new List<string>();
+
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+    }
+
+    public void CollectCode(string code)
+    {
+        collectedCodes.Add(code);
+        Debug.Log($"Collected Codes: {string.Join(", ", collectedCodes)}");
+    }
+
+    public bool HasCode(string code)
+    {
+        return collectedCodes.Contains(code);
+    }
+}
--- Chrono Escape/Assets/Script/Ticket.cs
+++ Chrono Escape/Assets/Script/Ticket.cs
@@ -1,6 +1,14 @@
-﻿using UnityEngine;
+﻿using System.Diagnostics;
 
 public class Ticket : MonoBehaviour
 {
-    public string code; // The unique code for this ticket
+    public string code;
+
+    public void Collect()
+    {
+        Debug.Log($"Ticket Collected: {code}");
+        // Logic to store the code (e.g., in a global list or inventory)
+        GameManager.Instance.CollectCode(code);
+        Destroy(gameObject); // Remove the ticket
+    }
 }
